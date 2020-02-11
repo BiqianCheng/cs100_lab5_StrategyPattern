7aac68920b0db2a9a261d46fa2ebe985f4966a4e
KatelynCheng <chengbiqian@foxmail.com>
2020-02-11 08:46:57 -0800

    from lab 4

---
+++ sub_test.hpp
@@ -0,0 +1,16 @@
+#ifndef __SUB_TEST_HPP__
+#define __SUB_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "op.hpp"
+#include "sub.hpp"
+
+TEST(subTest, subEvaluateNonZero) {
+    Op* value1 = new Op(12);
+    Op* value2 = new Op(8);
+    Sub* test = new Sub(value1, value2);
+    EXPECT_EQ(test->evaluate(), 4);
+}
+
+#endif 
